860c7a68cd180fca9700cbb6ba71e6bbde4f4f1b
nesla <nes_92cl@hotmail.com>
2013-10-30 14:36:20 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Mejorandola</title>
+	<link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	
+	<script src="http://code.jquery.com/jquery.min.js"></script>
+	<script src="js/index.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
